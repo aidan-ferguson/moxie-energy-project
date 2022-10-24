2700dd0e3054e5fcdedc3f47fb3773fe0dbf1603
Aidan Ferguson <2566197f@student.gla.ac.uk>
2022-10-24 19:02:21 +0100

    #25 Created view for streaming CSV file data and added CSV to static files

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('test_connection', views.test_connection, name='test_connection'),
-    path('usage/electricity', views.electricity_usage, name='electricity_usage')
+    path('users/<user_id>/usage/electricity', views.electricity_usage, name='electricity_usage')
 ]
