1f1b57a4f904aa55bb3ab03a55385a69d4625e34
Aidan Ferguson <2566197f@student.gla.ac.uk>
2022-10-26 19:38:09 +0100

    Closes #32

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('test_connection', views.test_connection, name='test_connection'),
-    path('users/<user_id>/usage/electricity', views.electricity_usage, name='electricity_usage')
+    path('auth/csrf_token', views.get_csrf_token, name='get_csrf_token'),
+    path('auth/login', views.login_user, name='login'),
+    path('auth/logout', views.logout_user, name='logout'),
+    path('usage/electricity', views.electricity_usage, name='electricity_usage')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,8 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
-from utils import *
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware import csrf
+from api.utils import *
 
 TEST_DATA_CSV = "datasets/single_value_30m_intervals.csv"
 
@@ -8,8 +11,8 @@ def test_connection(request):
     return return_success({})
 
 # For now this is a dummy view that will read data from a CSV file and send a response
-def electricity_usage(request, user_id):
-    print(f"Request for electricity data for user id: {user_id}")
+def electricity_usage(request):
+    print(f"Request for electricity data for user id: {request.user.username}")
     
     # Open the file and store the 10 first readings in an array
     filepath = staticfiles_storage.path(TEST_DATA_CSV)
@@ -28,19 +31,37 @@ def electricity_usage(request, user_id):
             reading[columns[idx]] = value[idx]
         json_reading_values.append(reading)
         
-    return return_json({"success":True, "usage":json_reading_values})
+    return return_success({"usage":json_reading_values})
+
+# Function that will send a valid CSRF token to the client for inclusion in POST requests
+def get_csrf_token(request):
+    return return_success({"token": csrf.get_token(request)})
 
 # Login endpoint
-def login(request):
+def login_user(request):
     # Ensure the request is a POST request
     if request.method == 'POST':
         # Attempt to get the credentials from the request
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         
-        if username != None or password != None:
-            return return_json({"success":False})
+        if username == None or password == None:
+            return return_error("You must provide a username and password in the request")
         
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return return_success({})
+            else:
+                return return_error("This account has been disabled")
+        else:
+            return return_error("Invalid credentials")
         
     else:
-        return return_json({"success":False, "reason":"You must use a POST method to login"})
+        return return_error("You must use a POST method to login")
+    
+# Logout endpoint
+def logout_user(reqeust):
+    logout(reqeust)
+    return return_success({})
