da8e796c4fb46115b58d125cc90f20684a97ba48
Aidan Ferguson <2566197f@student.gla.ac.uk>
2022-11-07 23:15:04 +0000

    #40 moved ecosystem page to a seperate activity, with a button from the main one

--- frontend/main-app/app/src/main/java/com/sh22/frontend_networking_test/MainActivity.kt
+++ frontend/main-app/app/src/main/java/com/sh22/frontend_networking_test/MainActivity.kt
@@ -1,41 +1,22 @@
 package com.sh22.frontend_networking_test
 
-import android.app.PendingIntent.getActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import com.unity3d.player.UnityPlayer
 
 
 class MainActivity : AppCompatActivity() {
-    var mUnityPlayer: UnityPlayer? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Set a onClick callback for the test button
-        val button = findViewById<Button>(R.id.test_button)
+        val button = findViewById<Button>(R.id.ecosystem_button)
         button.setOnClickListener(View.OnClickListener {
-            UnityPlayer.UnitySendMessage("HealthManager", "SetHealth", "1.0")
-        });
-    }
-
-    override fun onDestroy() {
-        mUnityPlayer?.quit()
-        super.onDestroy()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mUnityPlayer?.pause()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mUnityPlayer?.resume()
+            val myIntent = Intent(this, EcosystemActivity::class.java)
+            startActivity(myIntent)
+        })
     }
 }
