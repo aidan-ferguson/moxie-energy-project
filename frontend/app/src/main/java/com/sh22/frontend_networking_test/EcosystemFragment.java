38d449517f1553e1c4fcae69225a816c3880a1f7
Aidan <29628351+aidan-ferguson@users.noreply.github.com>
2022-11-08 22:48:40 +0000

    #40 Finalised merge and cleaned up fragments

--- frontend/app/src/main/java/com/sh22/frontend_networking_test/EcosystemFragment.java
+++ frontend/app/src/main/java/com/sh22/frontend_networking_test/EcosystemFragment.java
@@ -59,6 +59,6 @@ public class EcosystemFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ecosystem2, container, false);
+        return inflater.inflate(R.layout.fragment_ecosystem, container, false);
     }
 }
--- frontend/app/src/main/java/com/sh22/frontend_networking_test/MainActivity.java
+++ frontend/app/src/main/java/com/sh22/frontend_networking_test/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sh22.frontend_networking_test;
 
+import android.graphics.LightingColorFilter;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.ColorUtils;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,7 +15,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.sh22.frontend_networking_test.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
@@ -21,8 +26,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
 
+        // Navigation bar stuff
         binding.bottomNavigationView.setOnNavigationItemSelectedListener(item ->{
-
             switch(item.getItemId()){
                 case R.id.home:
                     replaceFragment(new HomeFragment());
@@ -43,6 +48,15 @@ public class MainActivity extends AppCompatActivity{
 
             return true;
         });
+
+        Integer progress = 40;
+        Float score = Float.valueOf(progress) / 100;
+        ProgressBar progressBar = findViewById(R.id.progress_bar);
+        TextView textView = findViewById(R.id.text_view_progress);
+        progressBar.setProgress(progress);
+        textView.setText(progress.toString());
+        int resultColor = ColorUtils.blendARGB(0xFF0000, 0x00FF00, score);
+        textView.getBackground().setColorFilter(new LightingColorFilter(resultColor, resultColor));
     }
 
     private void replaceFragment(Fragment fragment){
