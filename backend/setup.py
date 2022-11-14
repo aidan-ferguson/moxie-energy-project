7855e7fa5121dad611ef0eea1597f51f0f551496
Aidan Ferguson <2566197f@student.gla.ac.uk>
2022-11-14 22:20:54 +0000

    #48 Update setup and scripts names

--- backend/setup.py
+++ backend/setup.py
@@ -4,8 +4,8 @@ For:
 - downloading datasets
 
 """
-from setup.download_dale_data import *
-from setup.download_national_averages import *
+from scripts.download_dale_data import *
+from scripts.download_national_averages import *
 import os
 
 DATASET_FOLDER = os.path.realpath("./static/datasets")
@@ -25,4 +25,4 @@ if __name__ == "__main__":
     postcode_download_file(postcode_folder)
     print("Combining files")
     postcode_combine_files(postcode_folder)
-    print(f"Downloaded & Combined UK postcode usage datasets to {os.path.join(DATASET_FOLDER, 'postcodes')}")
+    print(f"Downloaded & Combined UK postcode usage datasets to {postcode_folder}")
