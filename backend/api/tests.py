4146557a12b52c2e29ab548cfd8d63739d93ef73
Aidan <2566197f@student.gla.ac.uk>
2023-01-08 13:37:12 +0000

    CI test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -6,4 +6,4 @@ class TestConnection(TestCase):
     def test_connection_success(self):
         response = self.client.get(urls.reverse('test_connection'))
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(json.loads(response.content)["success"], "Connection to server failed")
+        self.assertTrue(json.loads(response.content)["success"], "Connection to server failed.")
