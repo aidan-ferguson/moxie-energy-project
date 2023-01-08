bc3176e2087c85b615f056b6c67bcdb08968418e
Aidan <2566197f@student.gla.ac.uk>
2023-01-08 13:31:53 +0000

    Fixed CI error not including unity and testing triggering on folder change

--- frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/EcosystemFragment.java
+++ frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/EcosystemFragment.java
@@ -11,19 +11,19 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.sh22.energy_saver_app.backendhandler.BackendInterface;
-// import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayer;
 
 import org.json.JSONException;
 
 import java.io.IOException;
 import java.util.Objects;
 
-// // All comments in this file should have double 
+// Template file for when building with unity version
 
 public class EcosystemFragment extends Fragment {
 
     // // Unity player variables
-    // private static UnityPlayer mUnityPlayer = null;
+    private static UnityPlayer mUnityPlayer = null;
     FrameLayout frameLayoutForUnity;
 
     public EcosystemFragment() {
@@ -51,46 +51,46 @@ public class EcosystemFragment extends Fragment {
         float score = (float)(aggregate_daily / limit);
 
          // Create unity player and view
-        //  if(mUnityPlayer == null) {
-        //      mUnityPlayer = new UnityPlayer(getActivity());
-        //  }
+         if(mUnityPlayer == null) {
+             mUnityPlayer = new UnityPlayer(getActivity());
+         }
 
          // We need to remove any old parent
-        //  if(mUnityPlayer.getParent() != null) {
-        //      ((CardView)mUnityPlayer.getParent()).removeView(mUnityPlayer);
-        //  }
+         if(mUnityPlayer.getParent() != null) {
+             ((CardView)mUnityPlayer.getParent()).removeView(mUnityPlayer);
+         }
 
         View view = inflater.inflate(R.layout.fragment_ecosystem, container, false);
         this.frameLayoutForUnity = view.findViewById(R.id.frameLayoutForUnity);
-        // this.frameLayoutForUnity.addView(mUnityPlayer.getView(),
-        //         FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+        this.frameLayoutForUnity.addView(mUnityPlayer.getView(),
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
          // Initialise view
-        // mUnityPlayer.requestFocus();
-        // mUnityPlayer.windowFocusChanged(true);
-        // mUnityPlayer.resume();
+        mUnityPlayer.requestFocus();
+        mUnityPlayer.windowFocusChanged(true);
+        mUnityPlayer.resume();
         getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
          // Set the eco-score of the player
-        // UnityPlayer.UnitySendMessage("HealthManager", "SetHealth", String.valueOf(score));
+        UnityPlayer.UnitySendMessage("HealthManager", "SetHealth", String.valueOf(score));
         return view;
     }
 
     @Override
     public void onDestroy() {
-        // mUnityPlayer.pause();
+        mUnityPlayer.pause();
         super.onDestroy();
     }
 
     @Override
     public void onPause() {
-        // mUnityPlayer.pause();
+        mUnityPlayer.pause();
         super.onPause();
     }
 
     @Override
     public void onResume() {
-        // mUnityPlayer.resume();
+        mUnityPlayer.resume();
         super.onResume();
     }
 }
