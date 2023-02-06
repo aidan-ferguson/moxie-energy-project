580b70bbc3027f88d47c9e226d7170187eadc79d
Jamie <2560013v@student.gla.ac.uk>
2023-02-06 15:25:07 +0000

    Added Log in activity

--- frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/SignInActivity.java
+++ frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/SignInActivity.java
@@ -1,15 +1,72 @@
 package com.sh22.energy_saver_app;
 
-public class SignInActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 
-    protected <GoogleSignInOptions> void onCreate(){
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
+public class SignInActivity extends AppCompatActivity {
+    EditText email, password;
+    Button LoginButton;
 
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         email = findViewById(R.id.username);
+         password = findViewById(R.id.password);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                if (!isEmailValid(String.valueOf(email))){
+                    email.setError("Invalid Email or Password");
+                    return;
+                }
+                if (!isPasswordValid(String.valueOf(password))){
+                    password.setError("Invalid Email or Password");
+                    return;
+                }
+                boolean isAuthenticated = true;
+
+                if (isAuthenticated){
+                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(SignInActivity.this, "Log in failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+            public boolean isPasswordValid(String userPassword) {
+                if(userPassword == "1234"){
+                    //add password Authentication
+                    return true;
+                }
+                return false;
+            }
+
+            public boolean isEmailValid(String userEmail) {
+                if(userEmail == "Admin"){
+                    // add email authentication
+                    return true;
+                }
+                return false;
+            }
+
+
+        });
     }
+
 }
+
+
+
+
+
