b59a6f1bc62e0b114bbe49f3ddbbb2aabe0034e6
Jamie <2560013v@student.gla.ac.uk>
2023-02-20 12:08:46 +0000

    added simple ui testing for log in and log out functionality

---
+++ frontend/main-app/app/src/androidTest/java/com/sh22/energy_saver_app/MainActivityTest.kt
@@ -0,0 +1,36 @@
+package com.sh22.energy_saver_app
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sh22.energy_saver_app.ui.activites.MainActivity
+import com.sh22.energy_saver_app.ui.fragments.SettingsFragment
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Test
+    fun NavBarTest() {
+        // launches main activity and checks whether nav bar is in view
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.bottomNavigationView)).check(matches(isDisplayed()))
+    }
+
+    fun LogOutButtonTest(){
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val fragmentScenario = FragmentScenario.launch(SettingsFragment::class.java)
+    }
+}
