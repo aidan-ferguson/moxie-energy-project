353fa4a716f388b598ad6ef59ec039216d283c42
Aidan <2566197f@student.gla.ac.uk>
2023-03-01 13:00:19 +0000

    #91 Some backend unit tests

--- backend/api/tests/test_views.py
+++ backend/api/tests/test_views.py
@@ -1,9 +1,37 @@
 from django.test import TestCase
 from django import urls
 import json
+from rest_framework.authtoken.models import Token
+from django.test import Client, TestCase
+from api.models import User
+
+TEST_USERNAME = 'testuser@test.com'
+TEST_PASSWORD = "test_password"
+TEST_FIRSTNAME = "Test"
+TEST_LASTNAME = "User"
+test_user = User.objects.get_or_create(username=TEST_USERNAME, password=TEST_PASSWORD, email=TEST_USERNAME, first_name=TEST_FIRSTNAME, last_name=TEST_LASTNAME,  data_provider="DALE:house_4")[0]
+token = Token.objects.get_or_create(user=test_user)[0]
+TEST_CLIENT = Client(HTTP_AUTHORIZATION=f"Token {token}")
 
 class TestConnection(TestCase):
     def test_connection_unauthenticated_failure(self):
-        response = self.client.get(urls.reverse('test-connection'))
+        response = self.client.get(urls.reverse("test-connection"))
         self.assertEqual(response.status_code, 401)
         self.assertEqual(json.loads(response.content)["detail"], "Authentication credentials were not provided.")
+        
+    def test_connection_invalid_token(self):
+        response = self.client.get(urls.reverse("test-connection"), HTTP_AUTHORIZATION=f"Token invalid")
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(json.loads(response.content)["detail"], "Invalid token.")
+        
+    def test_connection_authenticated_success(self):
+        response = TEST_CLIENT.get(urls.reverse("test-connection"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)["data"]["message"], f"Hello, {test_user.username}")
+
+class RegisterTest(TestCase):    
+    def test_register_preexisting_user(self):
+        response = TEST_CLIENT.post(urls.reverse("register"), **{"username":TEST_USERNAME, "password":TEST_PASSWORD, "firstname":TEST_FIRSTNAME, "last_name":TEST_LASTNAME})
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)["non_field_errors"], {})
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('test-connection', views.TestView.as_view(), name='test-connection'),
-    path('auth/register', views.RegisterView.as_view(), name='login'),
+    path('auth/register', views.RegisterView.as_view(), name='register'),
     path('auth/logout', views.LogoutView.as_view(), name='logout'),
     path('usage/appliances', views.AppliancesView.as_view(), name='get_appliances'),
     path('usage/national-average', views.NationalAverageView.as_view(), name='national-average'),
