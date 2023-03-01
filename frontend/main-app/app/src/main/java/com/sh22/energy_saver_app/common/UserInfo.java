3008b8d48c1b9192b7d5d0c8b8d7f694beda7a35
Aidan <2566197f@student.gla.ac.uk>
2023-03-01 13:47:15 +0000

    Friend score update frontend

--- frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/common/UserInfo.java
+++ frontend/main-app/app/src/main/java/com/sh22/energy_saver_app/common/UserInfo.java
@@ -5,12 +5,14 @@ public class UserInfo {
     public String username;
     public String firstname;
     public String surname;
+    public Double energy_score;
 
-    public UserInfo(int _user_id, String _username, String _firstname, String _surname) {
+    public UserInfo(int _user_id, String _username, String _firstname, String _surname, Double _energy_score) {
         user_id = _user_id;
         username = _username;
         firstname = _firstname;
         surname = _surname;
+        energy_score = _energy_score;
     }
 
     public UserInfo() {
